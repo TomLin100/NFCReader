698acbf37641329fb4af46448e3af7951a028f57
TomLin100 <3011850@gmail.com>
2013-08-06 12:30:57 +0800

    解決xml/nfc_tech_list.xml的Bug

--- src/com/nfcreader/MainActivity.java
+++ src/com/nfcreader/MainActivity.java
@@ -3,8 +3,10 @@ package com.nfcreader;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動產生的方法 Stub
+		
 	}
 
 }
